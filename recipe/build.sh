f4a3af39483e5226c01f753b2053b83336da315c
Isuru Fernando <isuruf@gmail.com>
2025-03-27 17:01:43 -0500

    fix unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i.bak "s/CRCPBasic basic_struct/CRCPBasic_C basic_struct/g" $PREFIX/symengine/cwrapper.h
+sed -i.bak "s/CRCPBasic basic_struct/CRCPBasic_C basic_struct/g" $PREFIX/include/symengine/cwrapper.h
 set -o errexit -o pipefail
 export DISABLE_AUTOBREW=1
 export SymEngine_DIR=$PREFIX
